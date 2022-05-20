f394910acce932c0b3279542e72a4b9af49546a6
Zone <2493920954@qq.com>
2022-05-20 14:38:02 +0800

    .

--- src/components/layout/HeaderAvatar.vue
+++ src/components/layout/HeaderAvatar.vue
@@ -2,16 +2,19 @@
     <div>
         <el-popover placement="bottom" trigger="hover">
             <div>
-                <el-link href="/my">我的</el-link>
+                <el-link :underline="false" href="/my">我的</el-link>
             </div>
             <div>
-                <el-link href="/logout">退出登录</el-link>
+                <el-link :underline="false" href="/message">消息</el-link>
+            </div>
+            <div>
+                <el-link :underline="false" href="/logout">退出登录</el-link>
             </div>
             <el-avatar
                 slot="reference"
                 :src="$store.state.user.avatar"
-                :size="30"
-            ></el-avatar>
+                :size="30">
+            </el-avatar>
         </el-popover>
     </div>
 </template>
