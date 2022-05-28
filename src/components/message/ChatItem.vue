f4eee9b5b5eb3e4951e7a2161e78fe8000b9f060
Zone <2493920954@qq.com>
2022-05-28 20:10:53 +0800

    聊天信息

--- src/components/message/ChatItem.vue
+++ src/components/message/ChatItem.vue
@@ -1,9 +1,21 @@
 <template>
-  <el-row v-if="item.from.userId == my.userId">
-    我发的 {{item}}
+  <el-row v-if="item.from.userId == my.userId" type="flex" justify="end">
+    <el-card class="msg-my">
+      {{item.content}}
+    </el-card>
+    <el-col :span="2">
+      <el-avatar :src="item.from.avatar"></el-avatar>
+      <div>{{item.from.nickname}}</div>
+    </el-col>
   </el-row>
-  <el-row v-else>
-    他发的
+  <el-row v-else type="flex" justify="start">
+    <el-col :span="2">
+      <el-avatar :src="item.from.avatar"></el-avatar>
+      <div>{{item.from.nickname}}</div>
+    </el-col>
+    <el-card class="msg">
+      {{item.content}}
+    </el-card>
   </el-row>
 </template>
 
@@ -23,5 +35,20 @@ export default {
 </script>
 
 <style scoped>
+.el-row {
+  margin-top: 10px;
+}
+.el-card {
+  border-radius: 10px;
+}
+.msg-my {
+  margin-right: 10px;
+  text-align: start;
+}
 
+.msg {
+  margin-left: 10px;
+  text-align: start;
+  max-width: max-content-100px;
+}
 </style>
